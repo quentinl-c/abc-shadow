f8fa9fb7076cfb424cada8725bc3704b8cc9dfcd
Quentin Laporte-Chabasse <quentin.laporte-chabasse@inria.fr>
2018-11-08 16:33:14 +0100

    [+] implem of mh post sampler for binom dist

--- abc_shadow/mh_impl.py
+++ abc_shadow/mh_impl.py
@@ -1,32 +1,33 @@
 import numpy as np
 import random
-from math import exp, pi
+from math import exp, pi, factorial
 
 from .abc_impl import get_candidate
 
 
-def mh_post_sampler(theta_0, y, delta, n, iters, ratio_fctn):
+def mh_post_sampler(theta_0, y, delta, n, iters, ratio_fctn, mask=None):
     posteriors = list()
 
     theta_res = theta_0
     posteriors.append(theta_res)
 
     for i in range(iters):
-        theta_res = mh_algo(theta_res, y, delta, n, ratio_fctn)
-        posteriors.append(theta_res)
         if i % 10 == 0:
             msg = "🔎  Iteration {} / {} n : {} theta {}".format(
                 i, iters, n, theta_res)
             print(msg)
 
+        theta_res = mh_algo(theta_res, y, delta, n, ratio_fctn, mask=mask)
+        posteriors.append(theta_res)
+
     return posteriors
 
 
-def mh_algo(theta_0, y, delta, n, ratio_fctn):
+def mh_algo(theta_0, y, delta, n, ratio_fctn, mask=None):
     theta_res = np.array(theta_0)
 
     for _ in range(n):
-        candidate = get_candidate(theta_res, delta)
+        candidate = get_candidate(theta_res, delta, mask)
         density_ratio = ratio_fctn(y, theta_res, candidate)
         prob = random.uniform(0, 1)
 
@@ -37,7 +38,7 @@ def mh_algo(theta_0, y, delta, n, ratio_fctn):
 
 
 """
-Ration Fonction (depending on which kind of model we want to sample)
+Ratio Fonction (depending on which kind of model we want to sample)
 """
 
 
@@ -51,6 +52,22 @@ def norm_ratio(y, theta, candidate):
     ratio = q * exp(-0.5*(p1 - p2))
     return ratio
 
+def _binom_likelihood(n, p, k):
+    res1 = factorial(int(n)) / (factorial(int(k)) * factorial(int(n - k)))
+    res2 = p**(k) * (1 - p)**(n - k)
+    res = res1 * res2
 
-def binom_ratio():
-    pass
+    return res
+
+def binom_ratio(y, theta, candidate):
+    n = theta[0]
+    p_theta = theta[1]
+    p_candidate = candidate[1]
+    k = y[0]
+
+    q1 = _binom_likelihood(n, p_candidate, k)
+    q2 = _binom_likelihood(n, p_theta, k)
+    ratio = q1 / q2
+
+    return ratio
+
