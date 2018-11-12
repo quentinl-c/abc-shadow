abbc1f4f4590a9915bcbc23bab06fa9c7af4568c
Quentin Laporte-Chabasse <quentin.laporte-chabasse@inria.fr>
2018-11-12 17:59:23 +0100

    [U] script to test samplers

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from metropolis_hasting.graph.graph_wrapper import GraphWrapper
-from metropolis_hasting.metropolis_hasting import metropolis_hasting
-from metropolis_hasting.model.dyadic_model import DyadicModel
-from metropolis_hasting.util import (dyad_summary,
-                                     display,
-                                     markov_summary,
-                                     dist_display)
-from metropolis_hasting.model.markov_model import MarkovModel
+from abc_shadow.graph.graph_wrapper import GraphWrapper
+from abc_shadow.sampler import mh_sampler
+from abc_shadow.model.binomial_graph_model import BinomialGraphModel
+from abc_shadow.utils import dist_display, display
+from abc_shadow.abc_impl import binom_graph_sampler
+import numpy as np
 
 
 def main():
-    m = MarkovModel(0.5, 0.5, 0.5, 0.5, 0.5, 0.5)
-    # m = DyadicModel(0.5, 0.5, 0.5)
-    g = GraphWrapper(20)
+    m = BinomialGraphModel(1,5)
+    g = GraphWrapper(10)
 
-    res = metropolis_hasting(g, m, iter=100)
-    dist_display(res, markov_summary, prefix='hist_')
+    res = mh_sampler(g, m, iters=100)
+    stats = m.summary(res)
+    print(" None edge count : {}; Edge count : {}".format(np.mean(stats['None edges counts']), np.mean(stats['Edges counts'])))
+    
+    print(binom_graph_sampler(m, 10, 100))
+    #dist_display(res, m, prefix='hist_')
     # edge_dyad_display(res, prefix='_')
-    display(res, markov_summary, "Markov Model", prefix='_')
+    #display(res, m, "Binomial Model", prefix='_')
     # qq_plot(res, prefix='qqplot')
 
 if __name__ == '__main__':
